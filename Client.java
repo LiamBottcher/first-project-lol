435d63850767dda4c09d2deb8eb7e9b9e09d1921
Liam Bottcher <lbottcher171@gmail.com>
2025-07-22 17:18:19 -0400

    all files tracked

---
+++ Client.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
+public class Client{
+
+	public static void main(String[] args){
+		try{
+			Socket socket = new Socket("127.0.0.1", 1337);
+			Connection connection = new Connection(socket);
+			new Thread(connection).start();
+			Scanner scanner = new Scanner(System.in);
+			while (true){
+				String message = scanner.nextLine();
+				connection.write(message);}
+		}catch(IOException e){}
+	}
+
+
+}
+
+class Connection implements Runnable{
+	Socket socket;
+	BufferedReader reader;
+	BufferedWriter writer;
+	@Override
+	public void run(){
+		while (true){
+			try{
+				String message = reader.readLine();
+				System.out.println(message);
+			}catch(IOException e){}
+		
+		}
+	}
+	
+
+	public Connection(Socket socket){
+		try{
+		this.socket = socket;
+		this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+	        this.writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+		}catch(IOException e){}
+	}
+	public void write(String message){
+		try{
+			writer.write(message + "\n");
+			writer.flush();
+		}catch(IOException e){}
+	}
+}
---
+++ Server.java
@@ -0,0 +1,109 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class Server{
+
+	public static List<ClientHandler> clients = Collections.synchronizedList(new ArrayList<>());
+	static int id = 0;
+	static ServerSocket ss;
+	public static void main(String[] args){
+
+
+		try{
+		ss = new ServerSocket(1337);
+		}
+
+		catch(IOException e){}
+		while (true){
+
+			try{
+
+				Socket socket = ss.accept();
+				ClientHandler client = new ClientHandler(socket, id++);
+				clients.add(client);
+				new Thread(client).start();
+				//new Thread(new Thing(socket, id++)).start(); old way doesn't work because I have to add it to synvhronizedList
+
+			}catch(IOException e){}
+
+		}
+
+	}
+
+}
+
+
+
+class ClientHandler implements Runnable{
+
+	Socket socket;
+	int id;
+	BufferedReader reader;
+	BufferedWriter writer;
+
+	ClientHandler(Socket socket, int id){
+
+		this.socket = socket;
+		this.id = id;
+		try{
+
+			this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			this.writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+		}catch (IOException e){}
+
+	}
+
+
+	public void writeToSelf(String msg, int incomingId){
+
+		try{
+
+			writer.write("User #" + incomingId + ": " + msg + "\n");
+			writer.flush();
+
+		} catch (IOException e) {}
+
+	}
+
+
+
+	@Override
+	public void run(){
+
+		try{
+
+			while (true){
+
+				String message = reader.readLine();
+
+				if (message == null){
+
+					closeEverything();
+					break;
+				}
+				else{
+
+					for (ClientHandler client : Server.clients){
+
+						client.writeToSelf(message, id);
+					}
+				}
+			}
+		} catch(IOException e){}
+	}
+
+
+
+
+
+
+	public void closeEverything(){
+		try{
+			if (socket != null) socket.close();
+			if (reader != null) reader.close();
+			if (writer != null) writer.close();
+		}catch(IOException e){}
+	}
+}
